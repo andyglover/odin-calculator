0e934cce06ab3239a56b616cfa91bd653b4b13b8
Andy Glover <andrew.l.glover@gmail.com>
2022-08-08 14:50:37 -0700

    Make buttons turn colors and add to field

--- src/script.js
+++ src/script.js
@@ -1,10 +1,12 @@
 const numberButtons = document.querySelectorAll('.numberButton');
 const operatorButtons = document.querySelectorAll('.operatorButton');
+const inputField = document.querySelector('.inputField');
 
 numberButtons.forEach((numberButton) => {
     numberButton.addEventListener(
         'click', () => {
-            console.log("number");
+            console.log(numberButton.id);
+            inputField.textContent += numberButton.id;
             }
     )
 })
@@ -12,7 +14,13 @@ numberButtons.forEach((numberButton) => {
 operatorButtons.forEach((operatorButton) => {
     operatorButton.addEventListener(
         'click', () => {
-            console.log("operator");
-            }
+            console.log(operatorButton.id);
+            colorButton(operatorButton,"grey");
+        }
     )
 })
+
+function colorButton (button,color){
+    button.style.backgroundColor = color;
+}
+//make button number appear in text field
