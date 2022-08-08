13e28abc551eae698951b4743854ab5d5faaf414
Andy Glover <andrew.l.glover@gmail.com>
2022-08-08 14:37:38 -0700

    Make buttons do logs

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!");
+const numberButtons = document.querySelectorAll('.numberButton');
+const operatorButtons = document.querySelectorAll('.operatorButton');
+
+numberButtons.forEach((numberButton) => {
+    numberButton.addEventListener(
+        'click', () => {
+            console.log("number");
+            }
+    )
+})
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener(
+        'click', () => {
+            console.log("operator");
+            }
+    )
+})
